c31dc340438dbd58cdfaa4dffb6a2d4fa18af550
Ben Meier <benmeier42@gmail.com>
2013-03-19 17:11:53 +0200

    Added SensorClient main class

---
+++ src/SensorClient.java
@@ -0,0 +1,20 @@
+
+/**
+ * At the moment this is the main class. Guessing that in future it could be run like:
+ * $ java SensorClient						( spawn a client to send stuff to the server on an auto port )
+ * $ java SensorClient --serverport 54321	( a specific server port )
+ * $ java SensorClient --comport 1			( a specific serial port )
+ * 
+ * So the first thing might be to do proper argument parsing.
+ * 
+ * Hopefully if we design this properly we can run multiple sensor clients at once without interfering.
+ *
+ */
+public class SensorClient {
+	
+	public static void main(String[] args) {
+
+
+	}
+
+}
