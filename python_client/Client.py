0de1ebcbf0d12363fa99f4eba1452ccb2ebc7ad7
steveryb <steveryb@gmail.com>
2013-04-06 15:46:50 +0200

    Client now supports graphing multiple graphs at once
    Graphing is now essentially done which completes the two larger offline functions. Need to wrap this in a driver for text interfacing and complete network code but not bad atm.

--- python_client/Client.py
+++ python_client/Client.py
@@ -1,18 +1,23 @@
-import json, webbrowser, os
+import json, webbrowser, os, datetime
 from punix import Punix
 RESULTS_DATA_PATH = "templates/results.html"
+GRAPH_DATA_PATH = "templates/graph.js"
 RESULTS_JSON_STRING = "@JSON_QUERY_DATA_STRING"
+RESULTS_FOOTER = "@FOOTER_CONTENTS"
+QUERY_NUMBER = "@QUERY_NUMBER"
+CONTAINER_TOKEN = "@CONTAINER_CONTENTS"
+GRAPH_TOKEN = "@HIGHCHARTS_SETUP"
 class Client:
     def __init__(self):
         self.port_address="/dev/ttyUSB0"
         self.serial_dump_path="./serialdump-linux"
         self.headings = "Temp,Light"
 
-    def generate_graphs(self,dates=[],data= [{"name":"",
-    "data":[]}],y_axis_legend="",title="",subtitle="",data_type="",open_browser=False):
+    def generate_graphs(self,data=[],open_browser=False):
         """
         Generate the json output required to render a line graph with highcharts.js
-
+        
+        Takes an array of dictionaries with entries:
         dates: array of strings naming the points on the x-axis the data points
         will correspond to
         data: an array of dictionaries of the form {"name:"",data:[]} where name is
@@ -23,29 +28,40 @@ class Client:
         subtitle: the subtitle to give the graph
         data_type: the data type the graph is showing, e.g. degrees celsius.
         """
-        data_dict = {"dates":  dates,
-        "data": data,
-        "y_axis_legend" : y_axis_legend,
-        "title" :title,
-        "subtitle" : subtitle,
-        "data_type" : data_type}
+        graph_source = ""
+        with open(GRAPH_DATA_PATH) as f:
+            for line in f:
+                graph_source += line;
+        contents_code = ""
+        graph_code = ""
+        for i in range(len(data)):
+            contents_code +=("<div id= graph_%i></div>")%(i)
+            graph_code += graph_source.replace(QUERY_NUMBER,str(i))
+
         results = ""
         with open(RESULTS_DATA_PATH,"r") as f:
             for line in f:
                 results += line
-        results = \
-        results.replace(RESULTS_JSON_STRING,"'"+json.dumps(data_dict)+"'")
+        replace_pairs = ((RESULTS_JSON_STRING,"'"+json.dumps(data)+"'"),
+                        (RESULTS_FOOTER,"Generated at " +str(datetime.datetime.now())),
+                        (CONTAINER_TOKEN, contents_code),
+                        (GRAPH_TOKEN, graph_code))
+        for a,b in replace_pairs:
+            results = results.replace(a,b)
+
         with open("results.html","w") as f:
             f.write(results)
+
         if open_browser:
-            print("Attempting to open web browser. If this fails, manually"+
+            print("Attempting to open web browser. If this fails, manually "+
             "open results.html")
             dir_path = os.path.split(os.path.realpath(__file__))[0]
             results_path = os.path.join(dir_path,"results.html")
             url = "file://"+results_path
-            webbrowser.open_new_tab(url)
-        print("Results saved as results.html. Please open with a web browser"+
-        " to view")
+            #webbrowser.open_new_tab(url)
+        else: 
+            print("Results saved as results.html. Please open with a web browser"+
+            " to view")
 
     def capture_data(self,duration,verbose=False,
                      output_to_file=False, output_path=""):
--- python_client/TestClient.py
+++ python_client/TestClient.py
@@ -1,5 +1,22 @@
 import unittest
 from Client import Client
+graph_data = {
+    "dates":['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    "data":[{
+                    "name": 'Group 3',
+                    "data": [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+                }, {
+                    "name": 'Group 9',
+                    "data": [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
+                }, {
+                    "name": 'Group 22',
+                    "data": [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
+    }],
+    "y_axis_legend": 'Temperature (\u00b0C)',
+    "title":'Temperature',
+    "subtitle":'Source: Collection 3 Data',
+    "data_type": '(\u00b0C)'}
 class TestClient(unittest.TestCase):
 
     def setUp(self):
@@ -7,27 +24,13 @@ class TestClient(unittest.TestCase):
 
     def test_results_output(self):
         #TODO: figure out how to test this
-        graph_data = {
-            "dates":['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-                            'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-            "data":[{
-                            "name": 'Group 3',
-                            "data": [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-                        }, {
-                            "name": 'Group 9',
-                            "data": [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
-                        }, {
-                            "name": 'Group 22',
-                            "data": [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
-            }],
-            "y_axis_legend": 'Temperature (\u00b0C)',
-            "title":'Temperature',
-            "subtitle":'Source: Collection 3 Data',
-            "data_type": '(\u00b0C)'}
-        self.c.generate_graphs(**graph_data)
-        self.c.generate_graphs(open_browser=True,**graph_data)
-
-
+        self.c.generate_graphs(data=[graph_data])
+        self.c.generate_graphs(data=[graph_data], open_browser=True)
+    
+    def test_results_multiple_output(self):
+        self.c.generate_graphs(data=[graph_data,graph_data])
+        self.c.generate_graphs(data=[graph_data,graph_data], open_browser=True)
+            
     # These require a sensor connected to run
     """
     def test_data_capture_return(self):
