328c63175a186d9f89f170630bec6aa3aee503a4
steveryb <steveryb@gmail.com>
2013-04-07 15:05:51 +0200

    Added framework for adding client commands
    All the menus have now been coded, all that now needs to be done is implement the actual features!

--- src/ClientDriver.py
+++ src/ClientDriver.py
@@ -50,7 +50,7 @@ def capture_data(client):
     "recording? (y/n): ").lower().startswith("y")
     
     # Capture the data
-    client.capture_data(choice,verbose=verbose,
+    return client.capture_data(choice,verbose=verbose,
                      output_to_file=write_to_file, output_path=file_path)
 
 def render_local_data(client):
@@ -61,11 +61,60 @@ def render_local_data(client):
                              " this data? (y/n)").lower().startswith("y")
     client.generate_graph_from_data_file(file_path, open_browser=open_browser)
 
+def upload_data(client, source_id):
+    """
+    Upload sensor data to the server from a variety of sources
+
+    source_id = 0: Get the data from a local file
+    source_id = 1: Get the data from a sensor
+    source_id = 2: Generate the data heuristically
+    """
+    pass
+
+
+def get_data(client, group_id = -1):
+    """Get the data of a specified group. 
+    
+    id = 0 means all groups
+    id = -1 means this will prompt you to choose a valid group
+    otherwise, try get the info for that group id
+    """
+    pass
+
+def get_raw_data(client):
+    """Get the answer to a raw SQL query"""
+    pass
+
+def get_logs(client,log_type):
+    """
+    Get log data from the server
+    log_type = 0: All logs
+    log_type = 1: Uploading logs
+    log_type = 2: Downloading logs
+    """
+    pass
+
 if __name__=="__main__":
     client = Client()
     while True:
+        upload_screen = ("Where do you want to upload data from?",
+            ("Local File","Sensor","Data Generated Heuristically"),
+            [upload_data]*3,
+            [(client,i) for i in range(3)])
+
+        download_screen = ("What data do you want to download?",
+            ("Get own data","Get another group's data","Get all data","Get raw Data"),
+            [get_data]*3+[get_raw_data],
+            ((client,2),(client,-1),(client,0),(client)))
+
+        get_log_screen = ("What log information do you want?",
+            ("All the logs", "All the upload logs", "All the download logs"),
+            [get_logs]*3,
+            [(client,i) for i in range(3)])
+
         opening_screen = ("Welcome to Sensor Data Client! What would you like to "+
-        "do?",
-        (["Capture Sensor Data","Display local data","Exit"]),
-        [capture_data,render_local_data,sys.exit],[[client],[client],[]])
+            "do?",
+            ("Capture Sensor Data","Display local data","Upload Data to Server","Download Data from Server","Get Server Logs","Exit"),
+            [capture_data,render_local_data,execute_screen,execute_screen,execute_screen,sys.exit],
+            ([client],[client],upload_screen,download_screen,get_log_screen,()))
         execute_screen(*opening_screen)
